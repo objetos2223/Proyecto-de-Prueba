c2b642e81f58472f616d7d32261a433399c41513
eloyhz <eloyhz@gmail.com>
2016-10-12 01:34:18 -0500

    Primera modificación
    Esta es la primera modificación

--- Crab.java
+++ Crab.java
@@ -8,23 +8,24 @@ public class Crab extends Actor
      * Act - do whatever the Crab wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act() 
+    public void act()
     {
         move(4);
         gira();
         come();
-    }    
-    
+    }
+
     public void gira()
     {
         if (Greenfoot.isKeyDown("left"))    {
             turn(-3);
         }
+        
         if (Greenfoot.isKeyDown("right"))    {
             turn(3);
         }
     }
-    
+
     public void come()
     {
         if (isTouching(Worm.class)) {
@@ -44,9 +45,3 @@ public class Crab extends Actor
         }
     }
 }
-
-
-
-
-
-
